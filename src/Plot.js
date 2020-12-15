c7f53da303073de3a7d74de613ce282a51b321c0
Heman Robinson <heman.robinson@gmail.com>
2020-12-15 18:15:04 -0500

    Symbol sizes.

--- src/Plot.js
+++ src/Plot.js
@@ -20,7 +20,7 @@ const Plot = ( props ) => {
     
     // Hook to draw on mounting, or on any other lifecycle update.
     useEffect(() => {
-        Plot.draw( height, ref, xScale, yScale, symbolSet, dataSet, size * size );
+        Plot.draw( height, ref, xScale, yScale, symbolSet, dataSet, size );
     });
     
     // Return the component.
@@ -30,17 +30,21 @@ const Plot = ( props ) => {
 // Draws the points.
 Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     
-    // Draw the points.  +0.5 minimizes anti-aliasing.
+    // Initialization.
     const svg = d3.select( ref.current );
     let data = Data.getValues( dataSet );
     
     // Preattentive symbols.
-    let symbolPlus = {
+    let symbolCircle = {
         draw: function( g, size ) {
-            let s = Math.round( Math.sqrt( size / Math.PI ));
-            if( s > 1 ) {
-                s++;
-            };
+            let s = ( size === 0 ) ? 0 : Math.floor( size / 2 ) + 0.5;
+            g.arc( 0, 0, s, 0, 2 * Math.PI );
+            g.closePath();
+        }
+    },
+    symbolPlus = {
+        draw: function( g, size ) {
+            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5;
             g.moveTo( -s,  0 );
             g.lineTo(  s,  0 );
             g.moveTo(  0,  s );
@@ -50,7 +54,7 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     },
     symbolX = {
         draw: function( g, size ) {
-            let s = Math.round( Math.sqrt( size / Math.PI ));
+            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5;
             g.moveTo( -s, -s );
             g.lineTo(  s,  s );
             g.moveTo( -s,  s );
@@ -58,9 +62,20 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
             g.closePath();
         }
     },
+    symbolTriangle = {
+        draw: function( g, size ) {
+            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5,
+                t = Math.round( s * Math.sin( Math.PI / 6 )),
+                u = Math.round( s * Math.cos( Math.PI / 6 ));
+            g.moveTo(  0, -s );
+            g.lineTo(  u,  t );
+            g.lineTo( -u,  t );
+            g.closePath();
+        }
+    },
     symbolAsterisk = {
         draw: function( g, size ) {
-            let s = Math.round( Math.sqrt( size / Math.PI )),
+            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5,
                 t = Math.round( s * Math.sin( Math.PI / 6 )),
                 u = Math.round( s * Math.cos( Math.PI / 6 ));
             g.moveTo(  0,  s );
@@ -71,18 +86,31 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
             g.lineTo(  u, -t );
             g.closePath();
         }
+    },
+    symbolSquare = {
+        draw: function( g, size ) {
+            let s = ( size === 0 ) ? 0 : Math.floor( size / 2 ) + 0.5;
+            g.moveTo(  s,  s );
+            g.lineTo(  s, -s );
+            g.lineTo( -s, -s );
+            g.lineTo( -s,  s );
+            g.closePath();
+        }
     };
+    
+    // Choose the symbol set.
     let symbols = ( symbolSet === "geometric" )
         ? d3.symbols.slice( 0, 6 )
-        : [ d3.symbolCircle, symbolPlus, symbolX, d3.symbolTriangle, symbolAsterisk, d3.symbolSquare ];
-    let symbol = d3.scaleOrdinal( data.map( datum => datum[ 0 ]), symbols.map( s => d3.symbol().type( s ).size( size )()));
+        : [ symbolCircle, symbolPlus, symbolX, symbolTriangle, symbolAsterisk, symbolSquare ];
+    let symbolSize = ( symbolSet === "geometric" ) ? size * size : size;
+    let symbol = d3.scaleOrdinal( data.map( datum => datum[ 0 ]), symbols.map( s => d3.symbol().type( s ).size( symbolSize )()));
     
+    // Draw the points.  +0.5 minimizes anti-aliasing.
     svg.selectAll( "*" ).remove();
     data.forEach(( datum ) => {
         svg.append( "path" )
         .attr( "d", symbol( datum[ 0 ]))
-        .attr( "transform", d => `translate( ${ Math.round( xScale( datum[ 2 ]))}, ${ Math.round( yScale( datum[ 1 ]))})` )
-        .attr( "r", size / 2 )
+        .attr( "transform", d => `translate( ${ Math.round( xScale( datum[ 2 ]) + 0.5 )}, ${ Math.round( yScale( datum[ 1 ]) + 0.5 )})` )
         .style( "fill", "none" )
         .style( "stroke", "black" );
     });
