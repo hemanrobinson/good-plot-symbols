adc8c729550b2b9acebc971b9ebc9c0022dac6f6
Heman Robinson <heman.robinson@gmail.com>
2020-12-16 09:24:38 -0500

    better

--- src/Plot.js
+++ src/Plot.js
@@ -37,26 +37,29 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     // Preattentive symbols.
     let symbolCircle = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size / 2 ) + 0.5;
+            let s = ( size === 0 ) ? 0 : Math.sqrt( size / Math.PI );
             g.arc( 0, 0, s, 0, 2 * Math.PI );
             g.closePath();
         }
     },
     symbolPlus = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5;
+            let s = Math.sqrt( size / Math.PI );
+            s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 2 ) - 0.5;
             g.moveTo( -s,  0 );
             g.lineTo(  s,  0 );
-            g.moveTo(  0,  s );
-            g.lineTo(  0, -s );
+            g.moveTo(  0, -s );
+            g.lineTo(  0,  s );
             g.closePath();
         }
     },
     symbolX = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5;
+            let s = Math.sqrt( size / Math.PI );
+            s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / ( 2 * Math.SQRT2 )) - 0.5;
             g.moveTo( -s, -s );
             g.lineTo(  s,  s );
+            g.closePath();
             g.moveTo( -s,  s );
             g.lineTo(  s, -s );
             g.closePath();
@@ -64,9 +67,10 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     },
     symbolTriangle = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5,
-                t = Math.round( s * Math.sin( Math.PI / 6 )),
-                u = Math.round( s * Math.cos( Math.PI / 6 ));
+            let s = Math.sqrt( size / Math.PI );
+            s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 3 ) + 0.5;
+            let t = Math.round( s * Math.sin( Math.PI / 6 )) + 0.5,
+                u = Math.round( s * Math.cos( Math.PI / 6 )) + 0.5;
             g.moveTo(  0, -s );
             g.lineTo(  u,  t );
             g.lineTo( -u,  t );
@@ -75,13 +79,16 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     },
     symbolAsterisk = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size ) + 0.5,
-                t = Math.round( s * Math.sin( Math.PI / 6 )),
-                u = Math.round( s * Math.cos( Math.PI / 6 ));
+            let s = Math.sqrt( size / Math.PI );
+            s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 3 ) + 0.5;
+            let t = Math.round( s * Math.sin( Math.PI / 6 )) + 0.5,
+                u = Math.round( s * Math.cos( Math.PI / 6 )) + 0.5;
             g.moveTo(  0,  s );
             g.lineTo(  0, -s );
+            g.closePath();
             g.moveTo( -u, -t );
             g.lineTo(  u,  t );
+            g.closePath();
             g.moveTo( -u,  t );
             g.lineTo(  u, -t );
             g.closePath();
@@ -89,7 +96,8 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     },
     symbolSquare = {
         draw: function( g, size ) {
-            let s = ( size === 0 ) ? 0 : Math.floor( size / 2 ) + 0.5;
+            let s = Math.sqrt( size / Math.PI );
+            s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 4 ) + 0.5;
             g.moveTo(  s,  s );
             g.lineTo(  s, -s );
             g.lineTo( -s, -s );
@@ -102,7 +110,8 @@ Plot.draw = ( height, ref, xScale, yScale, symbolSet, dataSet, size ) => {
     let symbols = ( symbolSet === "geometric" )
         ? d3.symbols.slice( 0, 6 )
         : [ symbolCircle, symbolPlus, symbolX, symbolTriangle, symbolAsterisk, symbolSquare ];
-    let symbolSize = ( symbolSet === "geometric" ) ? size * size : size;
+    let radius = ( size === 0 ) ? 0 : size + 0.5;
+    let symbolSize = Math.PI * radius * radius;
     let symbol = d3.scaleOrdinal( data.map( datum => datum[ 0 ]), symbols.map( s => d3.symbol().type( s ).size( symbolSize )()));
     
     // Draw the points.  +0.5 minimizes anti-aliasing.
