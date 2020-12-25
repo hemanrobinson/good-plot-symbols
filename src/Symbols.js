9240727864dadcc72b86b8dba776ab05d2592b61
Heman Robinson <heman.robinson@gmail.com>
2020-12-25 16:07:54 -0500

    anti-aliasing

--- src/Symbols.js
+++ src/Symbols.js
@@ -1,95 +1,96 @@
 // Equally weighted symbols with preattentive features.
+// When translating from (0,0), use integers to minimize anti-aliasing, as 0.5 is added.
 const Symbols = () => {
 }
     
 // Circle.
 Symbols.symbolCircle = {
-    draw: function( g, size ) {
-        let s = ( size === 0 ) ? 0 : Math.sqrt( size / Math.PI );                           // matches d3.symbolCircle
+    draw: function( g, size ) { if( size > 0 ) {
+        let s = Math.sqrt( size / Math.PI );                           // matches d3.symbolCircle
         g.arc( 0, 0, s, 0, 2 * Math.PI );
         g.closePath();
-    }
+    }}
 };
 
 // Plus.
 Symbols.symbolPlus = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 2 - 0.25 );                      // -0.25 accounts for center pixel.
-        g.moveTo( -s,  0 );
-        g.lineTo(  s,  0 );
-        g.moveTo(  0, -s );
-        g.lineTo(  0,  s );
+        s = Math.round( s * Math.PI / 2 - 0.25 );                      // -0.25 accounts for center pixel.
+        g.moveTo( 0.5 - s, 0.5 );
+        g.lineTo( 0.5 + s, 0.5 );
         g.closePath();
-    }
+        g.moveTo( 0.5, 0.5 + s );
+        g.lineTo( 0.5, 0.5 - s );
+        g.closePath();
+    }}
 };
 
 // X.
 Symbols.symbolX = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / ( 2 * Math.SQRT2 ) - 0.25 );     // -0.25 accounts for center pixel.
-        g.moveTo( -s, -s );
-        g.lineTo(  s,  s );
+        s = Math.round( s * Math.PI / ( 2 * Math.SQRT2 ) - 0.25 );     // -0.25 accounts for center pixel.
+        g.moveTo( 0.5 - s, 0.5 - s );
+        g.lineTo( 0.5 + s, 0.5 + s );
         g.closePath();
-        g.moveTo( -s,  s );
-        g.lineTo(  s, -s );
+        g.moveTo( 0.5 - s, 0.5 + s );
+        g.lineTo( 0.5 + s, 0.5 - s );
         g.closePath();
-    }
+    }}
 };
 
 // Triangle.
 Symbols.symbolTriangle = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 3 + 1 );                         // +1 accounts for overlap.
+        s = Math.round( s * Math.PI / 3 + 1 );                         // +1 accounts for overlap.
         let t = Math.round( s * Math.sin( Math.PI / 6 )),
             u = Math.round( s * Math.cos( Math.PI / 6 ));
-        g.moveTo(  0, -s );
-        g.lineTo(  u,  t );
-        g.lineTo( -u,  t );
+        g.moveTo( 0.5    , 0.5 - s );
+        g.lineTo( 0.5 + u, 0.5 + t );
+        g.lineTo( 0.5 - u, 0.5 + t );
         g.closePath();
-    }
+    }}
 };
 
 // Asterisk.
 Symbols.symbolAsterisk = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 3 + 1 );                         // +1 accounts for overlap.
+        s = Math.round( s * Math.PI / 3 + 1 );                         // +1 accounts for overlap.
         let t = Math.round( s * Math.sin( Math.PI / 6 )),
             u = Math.round( s * Math.cos( Math.PI / 6 ));
-        g.moveTo(  0,  s );
-        g.lineTo(  0, -s );
+        g.moveTo( 0.5, 0.5 + s );
+        g.lineTo( 0.5, 0.5 - s );
         g.closePath();
-        g.moveTo( -u, -t );
-        g.lineTo(  u,  t );
+        g.moveTo( 0.5 - u, 0.5 - t );
+        g.lineTo( 0.5 + u, 0.5 + t );
         g.closePath();
-        g.moveTo( -u,  t );
-        g.lineTo(  u, -t );
+        g.moveTo( 0.5 - u, 0.5 + t );
+        g.lineTo( 0.5 + u, 0.5 - t );
         g.closePath();
-    }
+    }}
 };
 
 // Square.
 Symbols.symbolSquare = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / 4 ) + 0.5;                       // +0.5 minimizes anti-aliasing.
+        s = Math.round( s * Math.PI / 4 ) + 0.5;
         g.moveTo(  s,  s );
         g.lineTo(  s, -s );
         g.lineTo( -s, -s );
         g.lineTo( -s,  s );
         g.closePath();
-    }
+    }}
 };
 
 // Diamond.
 Symbols.symbolDiamond = {
-    draw: function( g, size ) {
+    draw: function( g, size ) { if( size > 0 ) {
         let s = Math.sqrt( size / Math.PI );
-        s = ( size === 0 ) ? 0 : Math.round( s * Math.PI / ( 2 * Math.SQRT2 ) + 0.25 );     // +0.25 accounts for center pixel.
-        s -= 0.5;                                                                           // 0.5 draws to center of pixels
+        s = Math.round( s * Math.PI / ( 2 * Math.SQRT2 )) + 0.5;
         g.moveTo( 0.5, -s );
         g.lineTo( s, -0.5 );
         g.closePath();
@@ -102,7 +103,7 @@ Symbols.symbolDiamond = {
         g.moveTo( 1 - s, -0.5 );
         g.lineTo( 0.5, -s );
         g.closePath();
-    }
+    }}
 };
 
 // Symbol set.
